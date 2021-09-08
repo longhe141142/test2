0e39203d952b59c21da9674c5ae1244534b20528
longhe141142 <longnt1@vmodev.com>
2021-09-08 19:32:05 +0700

    Update MVC model 08/09

--- Homework/Day3/SQLSERVER/helper.js
+++
@@ -1,7 +0,0 @@
-const extractPe = (people) =>{
-    people.map((val,index,arr) => {
-        console.log(`User id: ${val.id} and User name: ${val.name}`)
-    },0)
-}
-
-module.exports = { extractPe }
